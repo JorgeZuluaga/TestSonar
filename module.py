2f9a39178a87059dd8d168fff728a32528661d98
Ubuntu <ubuntu@ip-172-31-94-234.ec2.internal>
2019-05-23 15:36:28 +0000

    Commit

--- module.py
+++ module.py
@@ -1,4 +1,24 @@
 import numpy
 
-class Function1(object):
-    pass
+class Series(object):
+
+    def __init__(self,N,a):
+        self.N=N
+        self.a=a
+
+    def get_value(self,x):
+        suma=0
+        for n in range(self.N):
+            suma+=self.a[n]*x**n
+        return suma
+
+    def get_derivative(self,x):
+        deriv=0
+        for n in range(1,self.N):
+            deriv+=n*self.a[n]*x**(n-1)
+        return deriv
+
+if __name__=="__main__":
+    s=Series(3,[1,1/2,1/6])
+    print(s.get_derivative(1))
+    
--- test_module.py
+++ test_module.py
@@ -1,10 +1,18 @@
 import module
+import unittest
 
-def test_function():
-    try:
-        f=Function()
-        return True
-    except NameError:
-        raise AssertionError("No class with this name")
-    except:
-        raise AssertionError("Other error")
+class Test(unittest.TestCase):
+
+    def test_function(self):
+        f=module.Series(0,[])
+        
+    def test_get_value(self):
+        s=module.Series(3,[1,1/2,1/6])
+        y=s.get_value(1.0)
+        self.assertAlmostEqual(y,1.67,places=2)
+
+    def test_get_derivative(self):
+        s=module.Series(3,[1,1/2,1/6])
+        y=s.get_derivative(1.0)
+        self.assertAlmostEqual(y,0.83,places=2)
+        
